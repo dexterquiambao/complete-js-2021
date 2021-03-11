504f5506d26f8e8e59def14c4502fe038d440bff
Dexter Quiambao <dexterquiambao@googlemail.com>
2021-03-11 15:54:16 +0000

    Testing typeof to identify data type

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,12 +1,16 @@
-let js = 'amazing';
-console.log(40 + 8 + 23 -10);
+// let js = 'amazing';
+// console.log(40 + 8 + 23 -10);
 
-let firstName = 'Dexter';
-console.log(firstName);
+// let firstName = 'Dexter';
+// console.log(firstName);
 
 // Variable lecture challenge
-let country = 'Philippines';
-let continent = 'South East Asia';
-let population = '30 million';
+// let country = 'Philippines';
+// let continent = 'South East Asia';
+// let population = '30 million';
 
-console.log('I am from the ' + country + ' in ' + continent + ', with a population of ' + population +  '.');
+// console.log('I am from the ' + country + ' in ' + continent + ', with a population of ' + population +  '.');
+
+console.log(typeof true);
+console.log(typeof 23);
+console.log(typeof 'Dexter');
