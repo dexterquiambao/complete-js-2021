e38b537d8edc1dc48993d8a59269b5622647b344
Dexter Quiambao <dexterquiambao@googlemail.com>
2021-03-12 12:00:09 +0000

    Data sets lecture challenge

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -5,12 +5,18 @@
 // console.log(firstName);
 
 // Variable lecture challenge
-// let country = 'Philippines';
-// let continent = 'South East Asia';
-// let population = '30 million';
+let country = 'Philippines';
+let continent = 'South East Asia';
+let population = '30 million';
 
-// console.log('I am from the ' + country + ' in ' + continent + ', with a population of ' + population +  '.');
+console.log('I am from the ' + country + ' in ' + continent + ', with a population of ' + population +  '.');
 
 console.log(typeof true);
 console.log(typeof 23);
 console.log(typeof 'Dexter');
+
+// Data sets lecture challenge
+let isIsland = true
+let language
+console.log(isIsland)
+console.log(language)
