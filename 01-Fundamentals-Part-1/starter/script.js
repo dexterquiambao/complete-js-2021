24903f1d559805e784c88cb87a39ba932871ea6c
Dexter Quiambao <dexterquiambao@googlemail.com>
2021-03-12 12:09:26 +0000

    Let const and var lecture challenge

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -5,18 +5,27 @@
 // console.log(firstName);
 
 // Variable lecture challenge
-let country = 'Philippines';
-let continent = 'South East Asia';
-let population = '30 million';
+// let country = 'Philippines';
+// let continent = 'South East Asia';
+// let population = '30 million';
 
-console.log('I am from the ' + country + ' in ' + continent + ', with a population of ' + population +  '.');
+// console.log('I am from the ' + country + ' in ' + continent + ', with a population of ' + population +  '.');
 
-console.log(typeof true);
-console.log(typeof 23);
-console.log(typeof 'Dexter');
+// console.log(typeof true);
+// console.log(typeof 23);
+// console.log(typeof 'Dexter');
 
 // Data sets lecture challenge
-let isIsland = true
-let language
-console.log(isIsland)
-console.log(language)
+// let isIsland = true
+// let language
+// console.log(isIsland)
+// console.log(language)
+
+// Let, const and var lecture
+// let language = 'English';
+// console.log(language);
+// language = 'Tagalog';
+// console.log(language);
+// const spokenLanguage = 'English';
+// spokenLanguage = 'Spanish'
+// script.js:30 Uncaught TypeError: Assignment to constant variable.
