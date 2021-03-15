f6460243d3e87b21eb8942f2b741906c233eb39d
Dexter Quiambao <dexterquiambao@googlemail.com>
2021-03-15 18:36:10 +0000

    Activating Strict Mode lecture

---
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,15 @@
+// Activating Strict Mode lecture
+
+'use strict'; // strict mode goes first
+
+let hasDriversLicense = false;
+const passTest = true;
+
+if (passTest) hasDriverLicense = true; // Introducing an error - missing 's'
+if (hasDriversLicense) console.log('I can drive');
+
+// const interface = 'Audio'; // reserved keywords so strict will tell you in console
+// const private = 534;
+
+
+
